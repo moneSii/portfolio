0df7b6ca4ce3e8ac477344f21ba49af21ed8181f
Simone <simoneilagan@outlook.com>
2025-09-22 14:28:40 -0400

    feat(projects): create placeholder projects

--- src/app/projects/project.model.ts
+++ src/app/projects/project.model.ts
@@ -1,7 +1,14 @@
 export interface project {
   id: number;
   name: string;
-  content: string;
+  content: content;
   status: 'Pre-Planning' | 'In-Progess' | 'Complete';
   version: string;
+}
+
+interface content {
+  summary: string;
+  features: string;
+  stack: string;
+  upcoming: string;
 }
--- src/app/projects/projects.service.ts
+++ src/app/projects/projects.service.ts
@@ -1,8 +1,51 @@
-import { Injectable } from '@angular/core';
+import { Injectable, WritableSignal, signal } from '@angular/core';
+
+import { project } from './project.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectsService {
-  
+  private projectMasterList: WritableSignal<project[]> = signal([
+    {
+      id: 0,
+      name: 'Pomomomo',
+      content: {
+        summary: 'yo',
+        features: 'you',
+        stack: 'you',
+        upcoming: 'uyo',
+      },
+      status: 'In-Progess',
+      version: '0.1.0',
+    },
+    {
+      id: 1,
+      name: 'Tackle',
+      content: {
+        summary: 'yo',
+        features: 'you',
+        stack: 'you',
+        upcoming: 'uyo',
+      },
+      status: 'In-Progess',
+      version: '0.1.0',
+    },
+    {
+      id: 2,
+      name: 'Orient',
+      content: {
+        summary: 'yo',
+        features: 'you',
+        stack: 'you',
+        upcoming: 'uyo',
+      },
+      status: 'In-Progess',
+      version: '0.1.0',
+    },
+  ]);
+
+  get projects() {
+    return this.projectMasterList.asReadonly();
+  }
 }
