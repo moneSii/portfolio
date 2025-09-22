8d39fe0f02476bb69a928d9f17c5f4eb9f2bacdd
Simone <simoneilagan@outlook.com>
2025-09-22 14:43:36 -0400

    feat(projects): display project content

--- src/app/projects/project/project.component.ts
+++ src/app/projects/project/project.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+import { project } from '../project.model';
 
 @Component({
   selector: 'app-project',
   imports: [],
   templateUrl: './project.component.html',
-  styleUrl: './project.component.css'
+  styleUrl: './project.component.css',
 })
 export class ProjectComponent {
-
+  project = input.required<project>();
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { ProjectComponent } from './project/project.component';
+
+import { ProjectsService } from './projects.service';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [ProjectComponent],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
+  private projectsService = inject(ProjectsService);
 
+  projectList = this.projectsService.projects;
 }
