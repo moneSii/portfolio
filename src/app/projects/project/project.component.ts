016ac523bdc0b0005a9aca84502664bfa24d3784
Simone <simoneilagan@outlook.com>
2025-09-29 15:21:15 -0400

    styling(projects): add style and animations to projects section

--- src/app/projects/project/project.component.ts
+++ src/app/projects/project/project.component.ts
@@ -1,5 +1,4 @@
-import { Component, input, signal } from '@angular/core';
-
+import { Component, input, signal, effect, OnInit } from '@angular/core';
 import { project } from '../project.model';
 
 @Component({
@@ -8,10 +7,34 @@ import { project } from '../project.model';
   templateUrl: './project.component.html',
   styleUrl: './project.component.css',
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit {
   project = input.required<project>();
 
   status = signal(false);
+  show = false;
+  firstInit = true;
+  cardSize = 'card';
+
+  constructor() {
+    effect(() => {
+      if (this.status()) {
+        this.cardSize = 'card inflate';
+      } else {
+        if (this.firstInit) {
+          this.cardSize = 'card';
+          this.firstInit = false;
+        } else {
+          this.cardSize = 'card deflate';
+        }
+      }
+    });
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.show = true;
+    }, this.project().id * 750);
+  }
 
   onPress() {
     this.status.update((val) => !val);
